7c64a6abdb58dac56d1a74f1e5a023ca06cdd00c
pwilliams-ck <pwilliams@cloudkey.io>
2026-06-20 13:36:45 -0500

    chore/nvim: gate prettier on project config
    Set vim.g.lazyvim_prettier_needs_config so conform runs Prettier only in repos that ship a Prettier config. Other formatters (gofmt, stylua, shfmt, rustfmt) keep formatting on save.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Only run Prettier in projects that have a Prettier config (respects each repo's intent)
+vim.g.lazyvim_prettier_needs_config = true
+
 vim.opt.mouse = ""
 
 -- Undercurl
