a47d5d4edafd151c7060ea7d6f29dbe00a04c93f
Parker Williams <152556103+parkrw@users.noreply.github.com>
2026-07-25 16:14:33 -0500

    nvim: add per-language indent defaults via ftplugin

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -14,8 +14,8 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 -- GUI Cursor
 vim.opt.guicursor = "n-v-c-sm:block,i-ci:blinkwait300-blinkon200-blinkoff150"
 
--- Tabs and spaces
-vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+-- Tabs and spaces (default 2; ftplugin/ overrides per language)
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
